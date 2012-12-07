3107288a66fb34f3b369f371cb6144b0be3dd02a
HyperGod <chris@localch.at>
2012-12-06 21:43:34 -0500

    Added Github web-hook serialization
    Should be finished

--- phpbb3-github.php
+++ phpbb3-github.php
@@ -1,8 +1,23 @@
 <?php
 
-$forum = 32;
-$user_id = 86;
-$password = "";
+$forum = 5;
+$user_id = 86; // chatbot in our case.
+
+if (!isset($_POST['payload'])) { die('woops'); }
+
+function get_gitio_url($url)
+{
+ $ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "http://git.io/");
+	curl_setopt($ch, CURLOPT_HEADER, true);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "url={$url}");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$exec = curl_exec($ch);
+	// NOTE: may need to be mofidifed if the link doesn't include all possible characters to match other regex solutions could also be implemented
+	preg_match('/http\:\/\/git\.io\/([a-zA-Z0-9_\-]+)/', $exec, $matches);
+	return $matches[0];
+}
 
 define('IN_PHPBB', true);
 $path = $_SERVER['DOCUMENT_ROOT']; 
@@ -27,26 +42,6 @@ $user->setup('common');
 /* Now we have a template for the user, insert chatbot details so it's under his account. Hackery! */
 $user->data = array_merge($user->data, $row);
 $auth->acl($user->data);
-
-$makesure = request_var('auth', '', true); 
-if ($makesure != $password) { echo('inc.'); die(); } // Just a bit of protection
-
-$recipient = utf8_normalize_nfc(request_var('', '', true));
- 
-$message = "
-[b][/b]: 
-";
-
-$time = time();
-$rawsubject = "  ";
-$my_subject   = utf8_normalize_nfc($rawsubject, '', true);
-$my_text   = utf8_normalize_nfc($message, '', true);
- 
-$poll = $uid = $bitfield = $options = '';
- 
-generate_text_for_storage($my_subject, $uid, $bitfield, $options, false, false, false);
-generate_text_for_storage($my_text, $uid, $bitfield, $options, true, true, true);
- 
 $data = array(
        'forum_id'      => $forum,
        'icon_id'      => false,
@@ -69,9 +64,34 @@ $data = array(
        'post_time'       => 0,
        'forum_name'      => '',
        'enable_indexing'   => true,
- 
 );
+
+$json_array = json_decode(stripslashes($_POST['payload']), true);
+$repository = $json_array['repository']['name'];
+foreach ($json_array['commits'] as $commit)
+{
+ $tiny_url = get_gitio_url(stripslashes($commit['url']));
+ $author = $commit['author']['username'];
+ $message = $commit['message'];
+ $post = "
+ [b]Component[/b]: $repository
+ [b]Link[/b]: $tiny_url
+ [b]Author[/b]: $author
+ [b]Commit Message[/b]: $message
+ ";
  
-submit_post('post', $my_subject, $user->data['username'], POST_NORMAL, $poll, $data);
+ $time = time();
+ $rawsubject = "[{$repository}] - {$author}";
+ $my_subject = utf8_normalize_nfc($rawsubject, '', true);
+ $my_text = utf8_normalize_nfc($post, '', true);
+ $poll = $uid = $bitfield = $options = '';
+  
+ generate_text_for_storage($my_subject, $uid, $bitfield, $options, false, false, false);
+ generate_text_for_storage($my_text, $uid, $bitfield, $options, true, true, true);
+   
+ submit_post('post', $my_subject, $user->data['username'], POST_NORMAL, $poll, $data);
+}
+
+
 
 ?>
