70d8d4ba4046e3f5a19ddef161df30ffb07da450
HyperGod <chris@localch.at>
2012-12-06 21:45:14 -0500

    Spacing/tab fixing

--- phpbb3-github.php
+++ phpbb3-github.php
@@ -7,7 +7,7 @@ if (!isset($_POST['payload'])) { die('woops'); }
 
 function get_gitio_url($url)
 {
- $ch = curl_init();
+	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, "http://git.io/");
 	curl_setopt($ch, CURLOPT_HEADER, true);
 	curl_setopt($ch, CURLOPT_POST, true);
@@ -43,53 +43,53 @@ $user->setup('common');
 $user->data = array_merge($user->data, $row);
 $auth->acl($user->data);
 $data = array(
-       'forum_id'      => $forum,
-       'icon_id'      => false,
- 
-       'enable_bbcode'      => true,
-       'enable_smilies'   => true,
-       'enable_urls'      => true,
-       'enable_sig'      => true,
- 
-       'message'      => $my_text,
-       'message_md5'   => md5($my_text),
-               
-       'bbcode_bitfield'   => $bitfield,
-       'bbcode_uid'      => $uid,
- 
-       'post_edit_locked'   => 0,
-       'topic_title'      => $my_subject,
-       'notify_set'      => false,
-       'notify'         => true,
-       'post_time'       => 0,
-       'forum_name'      => '',
-       'enable_indexing'   => true,
+    'forum_id'      => $forum,
+    'icon_id'      => false,
+    
+    'enable_bbcode'      => true,
+    'enable_smilies'   => true,
+    'enable_urls'      => true,
+    'enable_sig'      => true,
+    
+    'message'      => $my_text,
+    'message_md5'   => md5($my_text),
+           
+    'bbcode_bitfield'   => $bitfield,
+    'bbcode_uid'      => $uid,
+    
+    'post_edit_locked'   => 0,
+    'topic_title'      => $my_subject,
+    'notify_set'      => false,
+    'notify'         => true,
+    'post_time'       => 0,
+    'forum_name'      => '',
+    'enable_indexing'   => true,
 );
 
 $json_array = json_decode(stripslashes($_POST['payload']), true);
 $repository = $json_array['repository']['name'];
 foreach ($json_array['commits'] as $commit)
 {
- $tiny_url = get_gitio_url(stripslashes($commit['url']));
- $author = $commit['author']['username'];
- $message = $commit['message'];
- $post = "
- [b]Component[/b]: $repository
- [b]Link[/b]: $tiny_url
- [b]Author[/b]: $author
- [b]Commit Message[/b]: $message
- ";
- 
- $time = time();
- $rawsubject = "[{$repository}] - {$author}";
- $my_subject = utf8_normalize_nfc($rawsubject, '', true);
- $my_text = utf8_normalize_nfc($post, '', true);
- $poll = $uid = $bitfield = $options = '';
-  
- generate_text_for_storage($my_subject, $uid, $bitfield, $options, false, false, false);
- generate_text_for_storage($my_text, $uid, $bitfield, $options, true, true, true);
-   
- submit_post('post', $my_subject, $user->data['username'], POST_NORMAL, $poll, $data);
+	$tiny_url = get_gitio_url(stripslashes($commit['url']));
+	$author = $commit['author']['username'];
+	$message = $commit['message'];
+	$post = "
+	[b]Component[/b]: $repository
+	[b]Link[/b]: $tiny_url
+	[b]Author[/b]: $author
+	[b]Commit Message[/b]: $message
+	";
+	
+	$time = time();
+	$rawsubject = "[{$repository}] - {$author}";
+	$my_subject = utf8_normalize_nfc($rawsubject, '', true);
+	$my_text = utf8_normalize_nfc($post, '', true);
+	$poll = $uid = $bitfield = $options = '';
+	
+	generate_text_for_storage($my_subject, $uid, $bitfield, $options, false, false, false);
+	generate_text_for_storage($my_text, $uid, $bitfield, $options, true, true, true);
+	
+	submit_post('post', $my_subject, $user->data['username'], POST_NORMAL, $poll, $data);
 }
 
 
