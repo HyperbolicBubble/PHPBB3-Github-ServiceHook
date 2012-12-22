552aae183c9aaa817865351be668f909c0373589
HyperGod <hypergod@hyperbolicbubble.com>
2012-12-22 16:24:52 -0500

    Script clean up, readme finished.
    Just a quick clean up of some minor whitespace issues on the file.
    Additionally the readme now explains the very basic process of getting
    this to run.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # PHPBB3-Github-ServiceHook
-A service hook for Github, it posts the commit as a new topic on your PHPBB3 installation.
+A service hook for Github, it posts a topic for each new commit.
 
 ## Getting started
-TODO: This    
+Upload this to your server where your forum is located. Edit the top three variables to your liking. I'd recommend renaming the file, too.
+
+Once that's done, go to your repo: `Settings -> Service Hooks -> WebHook URLs`
+
+Here you enter the URL of the file you just uploaded and save. Wollah! All new commits should now generate a topic.
--- phpbb3-github.php
+++ phpbb3-github.php
@@ -1,32 +1,28 @@
 <?php
 
-$forum = 5;
-$user_id = 86; // chatbot in our case.
+$forum = 5; // The ID of the forum, you'd like to make the new topics in.
+$user_id = 86; // ID of the user you'd like to post as.
+$path = $_SERVER['DOCUMENT_ROOT'] . '/forum/';  // Path to your forum installation.
 
 if (!isset($_POST['payload'])) { die('woops'); }
-// 207.97.227.253, 50.57.128.197, 108.171.174.178.
-if (!($_SERVER["REMOTE_ADDR"] == "207.97.227.253" || $_SERVER["REMOTE_ADDR"] == "50.57.128.197" || $_SERVER["REMOTE_ADDR"] == "108.171.174.178")) {
-    die('.');
-}
+if (!($_SERVER["REMOTE_ADDR"] == "207.97.227.253" || $_SERVER["REMOTE_ADDR"] == "50.57.128.197" || $_SERVER["REMOTE_ADDR"] == "108.171.174.178")) { die('.'); }
 
-function get_gitio_url($url)
-{
-    $ch = curl_init();
+function get_gitio_url($url) {
+	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, "http://git.io/");
 	curl_setopt($ch, CURLOPT_HEADER, true);
 	curl_setopt($ch, CURLOPT_POST, true);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, "url={$url}");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$exec = curl_exec($ch);
-	// NOTE: may need to be mofidifed if the link doesn't include all possible characters to match other regex solutions could also be implemented
+	// NOTE: may need to be modified if the link doesn't include all possible characters to match other regex solutions could also be implemented
 	preg_match('/http\:\/\/git\.io\/([a-zA-Z0-9_\-]+)/', $exec, $matches);
 	return $matches[0];
 }
 
 define('IN_PHPBB', true);
-$path = $_SERVER['DOCUMENT_ROOT']; 
-$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : $path.'/forum/'; 
-$phpbb_admin_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : $path.'/forum/';
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : $path; 
+$phpbb_admin_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : $path;
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
 include($phpbb_root_path . 'includes/functions_posting.' . $phpEx);
@@ -49,8 +45,8 @@ $auth->acl($user->data);
 
 $json_array = json_decode($_POST['payload'], true);
 $repository = $json_array['repository']['name'];
-foreach ($json_array['commits'] as $commit)
-{
+
+foreach ($json_array['commits'] as $commit) {
 	$tiny_url = get_gitio_url(stripslashes($commit['url']));
 	$author = $commit['author']['name'];
 	$message = $commit['message'];
@@ -59,7 +55,7 @@ foreach ($json_array['commits'] as $commit)
 	[b]Link[/b]: $tiny_url
 	[b]Author[/b]: $author
 	[b]Commit message[/b]: $message
-    ";
+	";
 	
 	$time = time();
 	$rawsubject = "[{$repository}] - {$author}";
@@ -69,30 +65,26 @@ foreach ($json_array['commits'] as $commit)
 	generate_text_for_storage($my_subject, $uid, $bitfield, $options, false, false, false);
 	generate_text_for_storage($post, $uid, $bitfield, $options, true, true, true);
 	
-    $data = array(
-        'forum_id'      => $forum,
-        'icon_id'      => false,
-        
-        'enable_bbcode'      => true,
-        'enable_smilies'   => true,
-        'enable_urls'      => true,
-        'enable_sig'      => true,
-        
-        'message'      => $post,
-        'message_md5'   => md5($post),
-               
-        'bbcode_bitfield'   => $bitfield,
-        'bbcode_uid'      => $uid,
-        
-        'post_edit_locked'   => 0,
-        'topic_title'      => $my_subject,
-        'notify_set'      => false,
-        'notify'         => true,
-        'post_time'       => 0,
-        'forum_name'      => '',
-        'enable_indexing'   => true,
-    );
-    
+	$data = array(
+		'forum_id'      => $forum,
+		'icon_id'      => false,
+		'enable_bbcode'      => true,
+		'enable_smilies'   => true,
+		'enable_urls'      => true,
+		'enable_sig'      => true,
+		'message'      => $post,
+		'message_md5'   => md5($post),
+		'bbcode_bitfield'   => $bitfield,
+		'bbcode_uid'      => $uid,
+		'post_edit_locked'   => 0,
+		'topic_title'      => $my_subject,
+		'notify_set'      => false,
+		'notify'         => true,
+		'post_time'       => 0,
+		'forum_name'      => '',
+		'enable_indexing'   => true,
+	);
+
 	submit_post('post', $my_subject, $user->data['username'], POST_NORMAL, $poll, $data);
 }
 
